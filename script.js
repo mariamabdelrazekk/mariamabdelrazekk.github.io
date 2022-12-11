c0e291f618fc13878d1b28db586cf67da1494d6b
mariamabdelrazekk <91086698+mariamabdelrazekk@users.noreply.github.com>
2022-12-11 06:00:57 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var showCurrentTime = function()
+{
+    // display the string on the webpage
+    var clock = document.getElementById('clock');
+ 
+    var currentTime = new Date();
+ 
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridian = "AM";
+ 
+    // Set hours
+	  if (hours >= noon)
+	  {
+		  meridian = "PM";
+	  }
+
+	  if (hours > noon)
+	  {
+		  hours = hours - 12;
+	  }
+ 
+    // Set Minutes
+    if (minutes < 10)
+    {
+        minutes = "0" + minutes;
+    }
+ 
+    // Set Seconds
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds;
+    }
+ 
+    // put together the string that displays the time
+    var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
+ 
+    clock.innerText = clockTime;
+};
+
+// Getting the clock to increment on its own and change out messages and pictures
+var updateClock = function() 
+{
+  var time = new Date().getHours();
+  var messageText;
+  var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+}
+
+// Getting the clock to increment once a second
+var oneSecond = 1000;
+setInterval( updateClock, oneSecond);
